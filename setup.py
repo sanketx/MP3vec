61500be263239b9be483f87b49c7e24759b4d691
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2019-09-26 17:10:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 
 	name = "mp3vec",
-	version = '0.0.1',
+	version = '0.1.0',
 	description = "A Transferable Feature Representation Method for Protein Sequences",
 	url = "https://github.com/sanketx/MP3vec",
 	author = "Sanket Rajan Gupte",
